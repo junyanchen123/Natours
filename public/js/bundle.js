9b13934645b7ffa0403895915571c5ed55acedfe
Junyan Chen <jchen522@buffalo.edu>
2023-09-18 09:52:48 -0400

    update signup

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8465,7 +8465,7 @@ var signup = function signup(_x, _x2, _x3, _x4) {
           _context.next = 3;
           return (0, _axios.default)({
             method: 'POST',
-            url: 'http://127.0.0.1:3000/api/v1/users/signup',
+            url: '/api/v1/users/signup',
             data: {
               name: name,
               email: email,
@@ -8739,7 +8739,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58355" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57219" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];

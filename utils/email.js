cc37d468aa03259a9f3e2261a87cef0d8feefc33
Junyan Chen <jchen522@buffalo.edu>
2023-09-18 21:06:30 -0400

    update

--- utils/email.js
+++ utils/email.js
@@ -22,11 +22,14 @@ module.exports = class Email {
       });
     }
 
-    return nodemailer.createTransport(
-      nodemailerSendgrid({
-        apiKey: process.env.SENDGRID_PASSWORD
-      })
-    )
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
   }
 
   // Send the actual email
